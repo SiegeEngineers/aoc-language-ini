d196023d7d1981a7e8d253e6459e8d10ee3637de
Renée Kooi <renee@kooi.me>
2019-08-10 14:59:11 +0200

    Support top level language.ini

--- main.c
+++ main.c
@@ -23,13 +23,16 @@ __declspec(dllexport) void mmm_setup(mmm_mod_info* info) {
 __declspec(dllexport) void mmm_before_setup(mmm_mod_info* info) {
   aoc_ini_init();
 
-  if (info->meta->mod_base_dir[0] != '\0') {
-    char ini_path[MAX_PATH];
-    // mod_base_dir contains trailing '\'
-    sprintf(ini_path, "%s%s", info->meta->mod_base_dir, "language.ini");
-    if (file_exists(ini_path)) {
-      aoc_ini_load_strings(ini_path);
-    }
+  const char* mod_base_dir = info->meta->mod_base_dir;
+  if (mod_base_dir == NULL || mod_base_dir[0] == '\0') {
+    mod_base_dir = info->meta->game_base_dir;
+  }
+  char ini_path[MAX_PATH];
+  // mod_base_dir contains trailing '\'
+  sprintf(ini_path, "%s%s", mod_base_dir, "language.ini");
+  printf("ini_path = %s\n", ini_path);
+  if (file_exists(ini_path)) {
+    aoc_ini_load_strings(ini_path);
   }
 }
 
